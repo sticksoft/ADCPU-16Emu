71922727e22ba14993e98bd5113905e7256fa003
Stick <retrogamingfan@hotmail.com>
2012-04-06 17:42:25 +0100

    Added an assembler.

--- src/uk/co/sticksoft/adce/MainActivity.java
+++ src/uk/co/sticksoft/adce/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.*;
 import android.view.*;
 import android.view.View.*;
 import android.widget.*;
+import uk.co.sticksoft.adce.asm.Assembler;
 import uk.co.sticksoft.adce.cpu.*;
 
 public class MainActivity extends Activity
@@ -14,6 +15,7 @@ public class MainActivity extends Activity
 	private TextView[] statusLabels;
 	private Button startButton, resetButton;
 	private boolean running;
+	private TextView log;
 	
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -60,23 +62,96 @@ public class MainActivity extends Activity
 		});
         lyt.addView(resetButton);
         
+        log = new TextView(this);
+        lyt.addView(log);
+        
         reset();
+        
+        testMyAssembler();
     }
+    
+    final String notchs_example_asm =
+    		"        ; Try some basic stuff\r\n" + 
+    		"                      SET A, 0x30              ; 7c01 0030\r\n" + 
+    		"                      SET [0x1000], 0x20       ; 7de1 1000 0020\r\n" + 
+    		"                      SUB A, [0x1000]          ; 7803 1000\r\n" + 
+    		"                      IFN A, 0x10              ; c00d \r\n" + 
+    		"                         SET PC, crash         ; 7dc1 001a [*]\r\n" + 
+    		"                      \r\n" + 
+    		"        ; Do a loopy thing\r\n" + 
+    		"                      SET I, 10                ; a861\r\n" + 
+    		"                      SET A, 0x2000            ; 7c01 2000\r\n" + 
+    		"        :loop         SET [0x2000+I], [A]      ; 2161 2000\r\n" + 
+    		"                      SUB I, 1                 ; 8463\r\n" + 
+    		"                      IFN I, 0                 ; 806d\r\n" + 
+    		"                         SET PC, loop          ; 7dc1 000d [*]\r\n" + 
+    		"        \r\n" + 
+    		"        ; Call a subroutine\r\n" + 
+    		"                      SET X, 0x4               ; 9031\r\n" + 
+    		"                      JSR testsub              ; 7c10 0018 [*]\r\n" + 
+    		"                      SET PC, crash            ; 7dc1 001a [*]\r\n" + 
+    		"        \r\n" + 
+    		"        :testsub      SHL X, 4                 ; 9037\r\n" + 
+    		"                      SET PC, POP              ; 61c1\r\n" + 
+    		"                        \r\n" + 
+    		"        ; Hang forever. X should now be 0x40 if everything went right.\r\n" + 
+    		"        :crash        SET PC, crash            ; 7dc1 001a [*]\r\n" + 
+    		"        \r\n" + 
+    		"        ; [*]: Note that these can be one word shorter and one cycle faster by using the short form (0x00-0x1f) of literals,\r\n" + 
+    		"        ;      but my assembler doesn't support short form labels yet.     ";
+    
+	final char[] notchs_example_assembled =
+	{
+		0x7c01, 0x0030, 0x7de1, 0x1000, 0x0020, 0x7803, 0x1000, 0xc00d,
+		0x7dc1, 0x001a, 0xa861, 0x7c01, 0x2000, 0x2161, 0x2000, 0x8463,
+		0x806d, 0x7dc1, 0x000d, 0x9031, 0x7c10, 0x0018, 0x7dc1, 0x001a,
+		0x9037, 0x61c1, 0x7dc1, 0x001a, 0x0000, 0x0000, 0x0000, 0x0000,
+	};
+	
+	private void log(String s)
+	{
+		log.append(s+'\n');
+	}
+	
+	private void testMyAssembler()
+	{
+		char[] output = new Assembler().assemble(notchs_example_asm);
+		
+		if (output == null)
+		{
+			log("Assembled output was null.");
+			return;
+		}
+		
+		int correct = 0, incorrect = 0;
+		for (int i = 0; i < output.length || i < notchs_example_assembled.length; i++)
+		{
+			char actual, target;
+			if (i < output.length) actual = output[i]; else actual = 0;
+			if (i < notchs_example_assembled.length) target = notchs_example_assembled[i]; else target = 0;
+			
+			if (actual == target)
+			{
+				log(String.format("%04x", (int)actual) + " == " + String.format("%04x", (int)target));
+				correct++;
+			}
+			else
+			{
+				log(String.format("%04x", (int)actual) + " != " + String.format("%04x", (int)target));
+				incorrect++;
+			}
+		}
+		log("Assembled example: "+correct+" correct, "+incorrect+" incorrect.");
+	}
     
     private void reset()
     {
 		stop();
     	cpu.reset();
     	
-    	char[] notchs_example =
-		{
-			0x7c01, 0x0030, 0x7de1, 0x1000, 0x0020, 0x7803, 0x1000, 0xc00d,
-			0x7dc1, 0x001a, 0xa861, 0x7c01, 0x2000, 0x2161, 0x2000, 0x8463,
-			0x806d, 0x7dc1, 0x000d, 0x9031, 0x7c10, 0x0018, 0x7dc1, 0x001a,
-			0x9037, 0x61c1, 0x7dc1, 0x001a, 0x0000, 0x0000, 0x0000, 0x0000,
-		};
+
     	
-    	System.arraycopy(notchs_example, 0, cpu.RAM, 0, notchs_example.length);
+    	System.arraycopy(notchs_example_assembled, 0, cpu.RAM, 0, notchs_example_assembled.length);
     	
     	updateInfo();
     }
---
+++ src/uk/co/sticksoft/adce/asm/Assembler.java
@@ -0,0 +1,348 @@
+package uk.co.sticksoft.adce.asm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import uk.co.sticksoft.adce.cpu.CPU;
+
+public class Assembler
+{
+	public Assembler() {}
+	
+	private String source;
+	private int sourceIndex;
+	
+	ArrayList<Character> machinecode = new ArrayList<Character>();
+	private HashMap<String, Integer> labels = new HashMap<String, Integer>();
+	private HashMap<Integer, String> labelUsages = new HashMap<Integer, String>();
+	
+	public char[] assemble(String s) { return assemble(s, new ArrayList<String>()); }
+	public char[] assemble(String s, ArrayList<String> out_messages)
+	{
+		source = s;
+		sourceIndex = 0;
+		
+		
+		String line = null;
+		for (;;)
+		{
+			// Read next chunk, unless there was some left over from last loop
+			if (line == null)
+			{
+				line = readLine();
+				if (line == null)
+					break; // That's all, folks!
+			}
+			
+			// Get rid of whitespace and ignore blank lines
+			line = line.trim();
+			if (line.length() == 0)
+			{
+				line = null; // Done with this line
+				continue;
+			}
+			
+			char first = line.charAt(0);
+			
+			// Comments
+			if (first == ';')
+			{
+				line = null;
+				continue;
+			}
+			
+			// Labels
+			if (first == ':')
+			{
+				line = readLabel(line.substring(1));
+				continue;
+			}
+			
+			// Assuming if we hit this point, can only be an instruction?
+			line = readInstruction(line);
+		}
+		
+		// Do labels
+		for (Integer i : labelUsages.keySet())
+		{
+			String label = labelUsages.get(i);
+			if (labels.containsKey(label))
+				machinecode.set(i.intValue(), Character.valueOf((char)labels.get(label).intValue()));
+			else
+				out_messages.add(i.toString() + ": Error: Label "+label+" not found!");
+		}
+		
+		// Convert the ArrayList into a char[].
+		// Come back, std::vector<unsigned short>; all is forgiven! <3
+		char[] data = new char[machinecode.size()];
+		for (int i = machinecode.size(); i-->0;)
+			data[i] = machinecode.get(i);
+		return data;
+	}
+	
+	private String readLine()
+	{
+		if (sourceIndex >= source.length())
+			return null;
+		
+		// Look for next line-ending character
+		// Should work with any combination of Unix, Mac or Windows endings
+		int cr = source.indexOf('\r', sourceIndex);
+		int lf = source.indexOf('\n', sourceIndex);
+		
+		if (cr == -1)
+			cr = source.length();
+		if (lf == -1)
+			lf = source.length();
+		
+		// Skip CRLF
+		if (lf == cr+1)
+			cr = lf;
+		
+		int to =  Math.min(cr,lf);
+		String ret = source.substring(sourceIndex, to);
+		
+		sourceIndex = to+1;
+		return ret;
+	}
+	
+	private String readLabel(String line)
+	{
+		if (line.length() == 0)
+			return null;
+		
+		int space = line.indexOf(' ');
+		if (space != -1)
+		{
+			labels.put(line.substring(0, space), Integer.valueOf(machinecode.size()));
+			return line.substring(space);
+		}
+		else
+		{
+			labels.put(line, Integer.valueOf(machinecode.size()));
+			return null;
+		}
+	}
+	
+	
+	private String readInstruction(String line)
+	{
+		if (line.length() < 3)
+			return null;
+		
+		int index = 0;
+		String opcode = line.substring(0, 3);
+		
+		for (int i = 1; i < CPU.Opcode.values().length; i++)
+		{
+			if (opcode.equalsIgnoreCase(CPU.Opcode.values()[i].name()))
+			{
+				// This is a basic instruction, and must have two comma-separated args
+				int comma = line.indexOf(',', 5);
+				if (comma == -1)
+					return null; // Oops
+				
+				// A is easy to get
+				String A = line.substring(4, comma);
+				
+				// Find B
+				int bstart = -1, bend = line.length();
+				for (int j = comma + 1; j < line.length(); j++)
+				{
+					if (bstart == -1)
+					{
+						if (!Character.isWhitespace(line.charAt(j)))
+							bstart = j;
+					}
+					else if (Character.isWhitespace(line.charAt(j)))
+					{
+						bend = j;
+						break;
+					}
+				}
+				
+				if (bstart == -1 || bstart > bend)
+					return null; // Oops
+				
+				// Here it is!
+				String B = line.substring(bstart, bend);
+								
+				writeBasicInstruction(i, A, B);
+				
+				// Deal with the rest of the line (if any)
+				if (bend < line.length())
+					return line.substring(bend);
+				else
+					return null;
+			}
+		}
+		
+		// TODO: Handle non-basic instructions
+		if (opcode.equals("JSR"))
+		{
+			// Find address
+			int start = -1, end = line.length();
+			for (int j = 4; j < line.length(); j++)
+			{
+				if (start == -1)
+				{
+					if (!Character.isWhitespace(line.charAt(j)))
+						start = j;
+				}
+				else if (Character.isWhitespace(line.charAt(j)))
+				{
+					end = j;
+					break;
+				}
+			}
+			
+			if (start == -1 || start > end)
+				return null; // Oops
+			
+			// Here it is!
+			String addr = line.substring(start, end);
+			writeNonBasicInstruction(1, addr);
+		}
+		
+		return null;
+	}
+	
+	private void writeNonBasicInstruction(int opcode, String arg)
+	{
+		// Add placeholder for instruction
+		int instr_addr = machinecode.size();
+		machinecode.add(Character.valueOf((char)0));
+		int argval = writeValue(arg);
+		
+		// Pack the opcode, a, and b
+		opcode = ((opcode & 0x3f) << 4) | ((argval & 0x3f) << 10);
+		
+		machinecode.set(instr_addr, Character.valueOf((char)opcode));
+	}
+	
+	private void writeBasicInstruction(int opcode, String A, String B)
+	{
+		// Add placeholder for instruction
+		int instr_addr = machinecode.size();
+		machinecode.add(Character.valueOf((char)0));
+		int a = writeValue(A);
+		int b = writeValue(B);
+		
+		// Pack the opcode, a, and b
+		opcode = opcode | ((a & 0x3f) << 4) | ((b & 0x3f) << 10);
+		
+		machinecode.set(instr_addr, Character.valueOf((char)opcode));
+	}
+	
+	private String[] valcodes =
+	{
+		"A", "B", "C", "X", "Y", "Z", "I", "J",
+		"[A]", "[B]", "[C]", "[X]", "[Y]", "[Z]", "[I]", "[J]",
+		null, null, null, null, null, null, null, null,
+		"POP", "PEEK", "PUSH", "SP", "PC", "O"
+	};
+	private int writeValue(String val)
+	{
+		// Quick check for registers etc., listed above
+		for (int i = 0; i < 16; i++)
+			if (val.equalsIgnoreCase(valcodes[i]))
+				return i;
+		for (int i = 24; i < 30; i++)
+			if (val.equalsIgnoreCase(valcodes[i]))
+				return i;
+		
+		if (val.startsWith("[") && val.endsWith("]") && val.length() > 2)
+		{
+			val = val.substring(1, val.length()-1);
+			int plus = val.indexOf('+');
+			
+			if (plus == -1) // No plus
+			{
+				try
+				{
+					int number = Integer.decode(val);
+					if (number < 0 || number > 0xFFFF)
+						return 0;
+					machinecode.add(Character.valueOf((char)number));
+					return CPU.Value._nextword.ordinal();
+				}
+				catch (NumberFormatException e)
+				{
+					// Not a number, might be a label...
+					labelUsages.put(machinecode.size(), val);
+					
+					// Leave a placeholder
+					machinecode.add(Character.valueOf((char)0));
+					return CPU.Value._nextword.ordinal();
+				}
+			}
+			else // Got a plus!
+			{
+				if (plus == val.length()-1)
+					return 0; // Oops
+				
+				// One of the args is a register.
+				String lhs = val.substring(0,plus).trim(), rhs = val.substring(plus+1,val.length()).trim();
+				int lregister = -1, rregister = -1;
+				for (int i = 0; i < 8; i++)
+				{
+					if (lhs.equalsIgnoreCase(valcodes[i]))
+						lregister = i;
+					if (rhs.equalsIgnoreCase(valcodes[i]))
+						rregister = i;
+				}
+				if ((lregister == -1) == (rregister == -1))
+					return 0; // Exactly one must be a register
+				
+				if (rregister == -1)
+				{
+					lhs = rhs; // Don't bother to swap
+					rregister = lregister;
+				}
+				
+				try
+				{
+					int number = Integer.decode(lhs);
+					if (number < 0 || number > 0xFFFF)
+						return 0;
+					machinecode.add(Character.valueOf((char)number));
+					return CPU.Value._nextwordA.ordinal() + rregister;
+				}
+				catch (NumberFormatException e)
+				{
+					// Not a number, might be a label...
+					labelUsages.put(machinecode.size(), lhs);
+					
+					// Leave a placeholder
+					machinecode.add(Character.valueOf((char)0));
+					return CPU.Value._nextwordA.ordinal() + rregister;
+				}
+				
+			}
+		}
+		else // No brackets
+		{
+			try
+			{
+				int number = Integer.decode(val);
+				if (number < 0 || number > 0xFFFF)
+					return 0;
+				
+				if (number < 0x20)
+					return CPU.Value.x00.ordinal() + number;
+				
+				machinecode.add(Character.valueOf((char)number));
+				return CPU.Value.nextword.ordinal();
+			}
+			catch (NumberFormatException e)
+			{
+				// Not a number, might be a label...
+				labelUsages.put(machinecode.size(), val);
+				
+				// Leave a placeholder
+				machinecode.add(Character.valueOf((char)0));
+				return CPU.Value.nextword.ordinal();
+			}
+		}
+	}
+}
